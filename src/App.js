0086e0ead35353b51382c3e0db856861cde52c0d
Josanu Andrei <andrei@quadra.md>
2024-06-12 18:16:51 +0300

    Quick creating stupid things

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: #f5f5f5;
+    height: 100%;
+`;
 
 function App() {
   return (
     <div className="App">
+      <Wrapper>
 
+      </Wrapper>
     </div>
   );
 }
