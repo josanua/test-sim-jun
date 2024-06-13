6f534151fa5d376539f110c09b4e9f3aeb1c7102
Josanu Andrei <andrei@quadra.md>
2024-06-13 15:12:13 +0300

    Detected correct query

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -1,8 +1,7 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 const client = new ApolloClient({
-    // uri: 'https://point.md/graphql',
-    uri: 'https://flyby-router-demo.herokuapp.com/',
+    uri: 'https://point.md/graphql',
     cache: new InMemoryCache(),
 });
 
@@ -11,33 +10,24 @@ const client = new ApolloClient({
 client
     .query({
         query: gql`
-      query GetLocations {
-        locations {
-          id
-          name
-          description
-          photo
-        }
-      }
+            {
+              contents(
+                project_id: "5107de83-f208-4ca4-87ed-9b69d58d16e1", 
+                lang: "ru", 
+                skip: 0, 
+                take: 10
+              ) {
+                id
+                project_id
+                url
+                title {
+                  short
+                }
+              }
+            }   
     `,
     })
     .then((result) => console.log(result));
-
-// {
-//     contents(
-//         project_id: "5107de83-f208-4ca4-87ed-9b69d58d16e1",
-//     lang: "ru",
-//     skip: 0,
-//     take: 10
-// ) {
-//     id
-//     project_id
-//     url
-//     title {
-//         short
-//     }
-// }
-// }
 
 const NewsList = () => {
     return (
